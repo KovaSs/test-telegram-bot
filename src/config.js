d985d616c0cb715e100fab15518baa6359e700dc
Denis Isaev / Денис Исаев <sr_relikt@mail.ru>
2017-08-08 03:24:00 +0300

    hotfix | fixed CI env

--- src/config.js
+++ src/config.js
@@ -4,6 +4,6 @@ require('dotenv').config({ path: path.resolve(process.cwd(), '.env') });
 const env = process.env;
 
 module.exports = {
-  TOKEN: env.TELEGRAM_TOKEN,
-  DB_URL: env.DB_URL
+  TOKEN: env.TELEGRAM_TOKEN || env.HD_TELEGRAM_TOKEN,
+  DB_URL: env.DB_URL || env.HD_DB_URL
 }
