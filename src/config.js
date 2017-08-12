c6cdaa1db7dbd5c58957dc61f889377afe82bfae
Denis Isaev / Денис Исаев <sr_relikt@mail.ru>
2017-08-12 03:24:00 +0300

    hotfix | check creating env variables

--- src/config.js
+++ src/config.js
@@ -2,7 +2,6 @@ const path = require('path');
 require('dotenv').config({ path: path.resolve(process.cwd(), '.env') });
 
 const env = process.env;
-console.log('env', env)
 const config = {
   TOKEN: env.TELEGRAM_TOKEN || env.HD_TELEGRAM_TOKEN,
   DB_URL: env.DB_URL || env.HD_DB_URL
